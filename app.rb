509746bf15b56804350e8502770185a2932d6e98
yijordan <yij@uchicago.edu>
2025-02-14 02:49:25 +0000

    did rock paper scissor routes

--- app.rb
+++ app.rb
@@ -1,8 +1,15 @@
 require "sinatra"
 require "sinatra/reloader"
 
+get("/") do
+  "
+  <h1>Howdy!</h1>
+  "
+end
+
 get("/rock") do
   moves = ["rock", "paper", "scissors"]
+  @our_move = "rock"
   @com_move = moves.sample
 
   if @com_move == "rock"
@@ -11,6 +18,38 @@ get("/rock") do
     @outcome = "lost"
   else
     @outcome = "won"
+  end
+
+  erb(:commentary)
+end
+
+get("/paper") do
+  moves = ["rock", "paper", "scissors"]
+  @our_move = "paper"
+  @com_move = moves.sample
+
+  if @com_move == "rock"
+    @outcome = "won"
+  elsif @com_move == "paper"
+    @outcome = "tied"
+  else
+    @outcome = "lost"
+  end
+
+  erb(:commentary)
+end
+
+get("/scissors") do
+  moves = ["rock", "paper", "scissors"]
+  @our_move = "scissors"
+  @com_move = moves.sample
+
+  if @com_move == "rock"
+    @outcome = "lost"
+  elsif @com_move == "paper"
+    @outcome = "won"
+  else
+    @outcome = "tied"
   end
 
   erb(:commentary)
